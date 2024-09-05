a0de356faffec6108559e4e949f18ff89d711209
gaseidon <danil_gasilow@mail.ru>
2024-09-05 23:13:38 +0300

    first commit

--- send_to_amocrm.php
+++ send_to_amocrm.php
@@ -1,43 +1,11 @@
 <?php
 // Функция для авторизации в AmoCRM
-function getAccessToken() {
-    // Настройки приложения AmoCRM
-    $client_id = 'your_client_id';
-    $client_secret = 'your_client_secret';
-    $redirect_uri = 'your_redirect_uri';
-    $authorization_code = 'your_authorization_code';
-    
-    $url = "https://your_amocrm_domain.amocrm.ru/oauth2/access_token";
-    
-    $data = [
-        'client_id' => $client_id,
-        'client_secret' => $client_secret,
-        'grant_type' => 'authorization_code',
-        'code' => $authorization_code,
-        'redirect_uri' => $redirect_uri,
-    ];
-    
-    $options = [
-        'http' => [
-            'header' => "Content-Type: application/json\r\n",
-            'method' => 'POST',
-            'content' => json_encode($data),
-        ]
-    ];
-    
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    $response = json_decode($result, true);
-    
-    return $response['access_token'];
-}
-
 // Функция для создания сделки и контакта в AmoCRM
 function createDealAndContact($name, $email, $phone, $price, $timeSpent) {
-    $access_token = getAccessToken();
-    
+    $access_token = file_get_contents('access_token.txt');
+    $your_amocrm_domain = 'danilgasilow';
     // URL для создания контакта
-    $contactsUrl = "https://your_amocrm_domain.amocrm.ru/api/v4/contacts";
+    $contactsUrl = "https://".$your_amocrm_domain.".amocrm.ru/api/v4/contacts";
     
     // Данные для создания контакта
     $contactData = [
@@ -75,7 +43,7 @@ function createDealAndContact($name, $email, $phone, $price, $timeSpent) {
     $contactId = $contactResponse['_embedded']['contacts'][0]['id'];
     
     // URL для создания сделки
-    $leadsUrl = "https://your_amocrm_domain.amocrm.ru/api/v4/leads";
+    $leadsUrl = "https://".$your_amocrm_domain.".amocrm.ru/api/v4/leads";
     
     // Данные для создания сделки
     $leadData = [
